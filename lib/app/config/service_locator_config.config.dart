1db73e8920ed9710df0f5cdbd4d5c153a1b218b9
StephaneAntonieto <stephaneantonieto@gmail.com>
2024-10-31 15:36:38 -0300

    Database models, user router n auth controller

--- lib/app/config/service_locator_config.config.dart
+++ lib/app/config/service_locator_config.config.dart
@@ -11,6 +11,7 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
+import '../../modules/user/controller/auth_controller.dart' as _i477;
 import '../database/database_connection_impl.dart' as _i311;
 import '../database/i_database_connection.dart' as _i77;
 import 'database_connection_configuration.dart' as _i32;
@@ -26,6 +27,7 @@ extension GetItInjectableX on _i174.GetIt {
       environment,
       environmentFilter,
     );
+    gh.factory<_i477.AuthController>(() => _i477.AuthController());
     gh.lazySingleton<_i77.IDatabaseConnection>(() =>
         _i311.DatabaseConnectionImpl(
             gh<_i32.DatabaseConnectionConfiguration>()));
--- lib/app/routers/router_configure.dart
+++ lib/app/routers/router_configure.dart
@@ -1,12 +1,21 @@
+import 'package:cuidapet_api/modules/user/user_router.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 import 'package:cuidapet_api/app/routers/i_router.dart';
 
 class RouterConfigure {
   final Router _router;
-  final List<IRouter> _routers = [];
+  final List<IRouter> _routers = [
+    UserRouter(),
+  ];
 
   RouterConfigure(this._router);
 
-  void configure() => _routers.forEach((r) => r.configure(_router));
+  // void configure() => _routers.forEach((r) => r.configure(_router));
+
+  void configure() {
+    for (var r in _routers) {
+      r.configure(_router);
+    }
+  }
 }
