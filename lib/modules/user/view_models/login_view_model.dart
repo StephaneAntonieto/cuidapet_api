db67edda0db30395430ef38686785cb883699f59
StephaneAntonieto <stephaneantonieto@gmail.com>
2024-11-06 10:25:16 -0300

    Login com email e password

--- lib/modules/user/view_models/login_view_model.dart
+++ lib/modules/user/view_models/login_view_model.dart
@@ -4,15 +4,28 @@ class LoginViewModel extends RequestMapping {
   late String login;
   late String password;
   late bool socialLogin;
+  late String avatar;
+  late String socialType;
+  late String socialKey;
   late bool supplierUser;
 
   LoginViewModel(super.dataRequest);
 
   @override
   void map() {
-    login = data['login'];
-    password = data['password'];
-    socialLogin = data['social_login'];
-    supplierUser = data['supplier_user'];
+    login = data['login'] ?? '';
+    password = data.containsKey('password') ? data['password'] : '';
+    socialLogin = data['social_login'] ?? false;
+    avatar = data.containsKey('avatar') ? data['avatar'] : '';
+    socialType = data.containsKey('social_type') ? data['social_type'] : '';
+    socialKey = data.containsKey('social_key') ? data['social_key'] : '';
+    supplierUser = data['supplier_user'] ?? false;
+    // login = data['login'];
+    // password = data['password'];
+    // socialLogin = data['social_login'];
+    // avatar = data['avatar'];
+    // socialType = data['social_type'];
+    // socialKey = data['social_key'];
+    // supplierUser = data['supplier_user'];
   }
 }
