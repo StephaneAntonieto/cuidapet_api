9877f74e6a3654dd439d8d95a2bde620acc0e69a
StephaneAntonieto <stephaneantonieto@gmail.com>
2024-12-26 16:47:23 -0300

    Buscar todos agendamentos pelo usuário logado

--- lib/modules/schedules/service/i_schedule_service.dart
+++ lib/modules/schedules/service/i_schedule_service.dart
@@ -1,7 +1,10 @@
+import 'package:cuidapet_api/entities/schedule.dart';
 import 'package:cuidapet_api/modules/schedules/view_models/schedule_save_input_model.dart';
 
 abstract interface class IScheduleService {
   Future<void> scheduleService(ScheduleSaveInputModel model);
 
   Future<void> changeStatus(String status, int scheduleId);
+
+  Future<List<Schedule>> findAllSchedulesByUser(int userId);
 }
